d3dbd377933f8d66057c62f708efeb4aeb10b437
kbventures <kmb_x@hotmail.com>
2021-06-21 17:09:34 +0200

    Changed color of hamburger menu

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ let linkCollection = document.getElementById('navigation');
 linkCollection.addEventListener('click', function () {
   navigationToggle.checked = false;
 });
+
+// Popup Stories will close when black background clicked;
+// &__close {
+//   &:link,
+//   &:visited {
+//     color: $color-white;
+//     position: absolute;
+//     top: 2.5rem;
+//     right: 2.5rem;
+//     font-size: 2.5rem;
+//     text-decoration: none;
+//     display: inline-block;
+//     transition: all 0.2s;
+//     line-height: 1;
+
+//     &:hover {
+//       color: $color-primary-dark;
+//     }
+//   }
+// }
